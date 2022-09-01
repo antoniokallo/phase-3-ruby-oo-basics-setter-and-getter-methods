5fb712e4ac9b5ed8838e5a7fa84c96759b1a2e4d
antoniokallo <antoniokallo@gmail.com>
2022-09-01 11:52:20 -0400

    commit tree 242e487fd255b6860bd8403e45ce4f8e6dc4159d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
 
+attr_accessor :name 
+attr_accessor :breed
+
 end
+
+rebecca = Dog.new
+rebecca.name = "rebecca"
+rebecca.breed = "poodle"
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-
+attr_accessor :name 
+attr_accessor :job
 end
+
+ye = Person.new 
+ye.name = "ye"
+ye.job = "artist"
